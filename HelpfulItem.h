7e82759d1c9c8a81f273270dec88f60db79593e1
bbgreene <>
2021-04-20 13:26:27 +0000

    deleted Items.h virtual {}. Compiles but output not the same

--- HelpfulItem.h
+++ HelpfulItem.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "Item.h"
-#include "Dwarf.h"
+//#include "Dwarf.h"
 #include "Character.h"
-#include "Paladin.h"
-#include "DragonSlayer.h"
-#include "Dragon.h"
+//#include "Paladin.h"
+//#include "DragonSlayer.h"
+//#include "Dragon.h"
 
 struct HelpfulItem : Item
 {
--- Item.cpp
+++ Item.cpp
@@ -1,5 +1,3 @@
-//#pragma once
-
 #include "Item.h"
 
 Item::~Item() {}
